baf948f028ca66b0b78ea44cc38f5373ba1c9911
VladimirKovaliev <kovaliev.v@list.ru>
2024-03-11 12:44:19 +0300

    add str and meta

--- users/models.py
+++ users/models.py
@@ -11,3 +11,10 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'Пользователь: {self.email}'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
