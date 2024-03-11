bdfc4d197c18bb9d29fe6f3adad53f3330bc9567
VladimirKovaliev <kovaliev.v@list.ru>
2024-03-11 12:43:53 +0300

    модель привычки

--- main/models.py
+++ main/models.py
@@ -1,21 +1,21 @@
-# from django.db import models
-#
-# from config import settings
-#
-# # Create your models here.
-#
-# NULLABLE = {
-#     'null': True,
-#     'blank': True,
-# }
-#
-# periods = (
-#     ('daily', 'Ежедневно'),
-#     ('weekly', 'Еженедельно'),
-# )
-#
-#
-# class Habit(models.Model):
-#     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='создатель', **NULLABLE)
-#     place = models.CharField(max_length=150, verbose_name='место', **NULLABLE)
-#     time = models.TimeField(auto_now=False, auto_now_add=False, verbose_name='время', **NULLABLE)
+from django.db import models
+
+from config import settings
+
+# Create your models here.
+
+NULLABLE = {
+    'null': True,
+    'blank': True,
+}
+
+periods = (
+    ('daily', 'Ежедневно'),
+    ('weekly', 'Еженедельно'),
+)
+
+
+class Habit(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='создатель', **NULLABLE)
+    place = models.CharField(max_length=150, verbose_name='место', **NULLABLE)
+    time = models.TimeField(auto_now=False, auto_now_add=False, verbose_name='время', **NULLABLE)
