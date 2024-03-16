07a1ab804eed1a426420a46ac84461095b18fc33
VladimirKovaliev <kovaliev.v@list.ru>
2024-03-16 16:00:34 +0300

    add tg_id

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+NULLABLE = {'blank': True, 'null': True}
+
 
 class User(AbstractUser):
     username = None
@@ -8,6 +10,8 @@ class User(AbstractUser):
     is_superuser = models.BooleanField(default=False, verbose_name='админ')
     is_staff = models.BooleanField(default=False, verbose_name='сотрудник')
     is_active = models.BooleanField(default=True, verbose_name='активный')
+    telegram_id = models.CharField(max_length=20, verbose_name='Телеграм', **NULLABLE)
+
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
